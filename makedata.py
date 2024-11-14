b66cdfcfdc70c4a2bf644a489ea958fddb44f4f8
Zach Perry <zachp123@byu.edu>
2024-11-13 17:16:59 -0700

    Update datatypes

--- makedata.py
+++ makedata.py
@@ -20,6 +20,9 @@ while i < (len(ids)): #initialize while loop to API request for each ID
     variable.drop(['realtime_start', 'realtime_end'],axis = 1, inplace = True) 
     variable.columns = ['Date', ids[i]] #drop and rename columns
 
+    if i == 5: # The first 4 values of the GDP ID are just '.' so they need to be removed
+        variable = variable.iloc[4:]
+
     if i == 0: # create dataframe of all variables
         df = pd.DataFrame(variable)
     else:
@@ -29,6 +32,12 @@ while i < (len(ids)): #initialize while loop to API request for each ID
 # rename columns to proper names
 df.columns = ['Date','Fin_Market_Interest_Rates', 'CPI', 'Num_of_Workers(thousands)','Unemployment_Rate',
 'Work_Partic_Rate_Women', 'GDP(billions)', 'Work_Partic_Rate_All', 'Real_GDP_per_Capita', 'Hourly_Wages_Private($)']
+
+# set each variable to the correct data type
+df = df.astype({'Fin_Market_Interest_Rates': float, 'CPI': float, 'Num_of_Workers(thousands)': int, 
+                'Unemployment_Rate' : float,'Work_Partic_Rate_Women' : float, 'GDP(billions)': float, 
+                 'Work_Partic_Rate_All' : float, 'Real_GDP_per_Capita' : float, 'Hourly_Wages_Private($)' : float})
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
 
 # Note: not every variable in my dataframe has a value for every row, because each variable has been tracked for a different amount of time
 
